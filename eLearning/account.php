2654fecb7238e7ddba48922478719dd4156deb03
Kittim1 <118587962+Kittim1@users.noreply.github.com>
2024-03-26 13:13:08 +0800

    Update account.php

--- eLearning/account.php
+++ eLearning/account.php
@@ -1,3 +1,4 @@
+account.php
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -61,6 +62,7 @@ echo '<span class="pull-right top title1" ><span class="log1"><span class="glyph
         <span class="icon-bar"></span>
       </button>
       <!-- <a class="navbar-brand" href="#"><b>Netcamp</b></a> -->
+      <a class="navbar-brand"href="camera.php"><b>Camera</b></a>
       <a class="navbar-brand"href="feedback.php"><b>Feedback</b></a>
     </div>
 
@@ -72,9 +74,9 @@ echo '<span class="pull-right top title1" ><span class="log1"><span class="glyph
       <ul class="nav navbar-nav">
         <li <?php if(@$_GET['q']==1) echo'class="active"'; ?> ><a href="account.php?q=1"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>&nbsp;Home<span class="sr-only">(current)</span></a></li>
         <li <?php if(@$_GET['q']==2) echo'class="active"'; ?>><a href="account.php?q=2"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>&nbsp;History</a></li>
-		<li <?php if(@$_GET['q']==3) echo'class="active"'; ?>><a href="account.php?q=3"><span class="glyphicon glyphicon-stats" aria-hidden="true"></span>&nbsp;Ranking</a></li>
+    <li <?php if(@$_GET['q']==3) echo'class="active"'; ?>><a href="account.php?q=3"><span class="glyphicon glyphicon-stats" aria-hidden="true"></span>&nbsp;Ranking</a></li>
     
-		</ul>
+    </ul>
             <form class="navbar-form navbar-left" role="search">
         <div class="form-group">
           <input type="text" class="form-control" placeholder="Search here">
@@ -100,21 +102,21 @@ echo  '<div class="panel"><div class="table-responsive"><table class="table tabl
 <tr><td><b>S.N.</b></td><td><b>Topic</b></td><td><b>Total question</b></td><td><b>Marks</b></td><td><b>Time limit</b></td><td></td></tr>';
 $c=1;
 while($row = mysqli_fetch_array($result)) {
-	$title = $row['title'];
-	$total = $row['total'];
-	$sahi = $row['sahi'];
+  $title = $row['title'];
+  $total = $row['total'];
+  $sahi = $row['sahi'];
     $time = $row['time'];
-	$eid = $row['eid'];
+  $eid = $row['eid'];
 $q12=mysqli_query($con,"SELECT score FROM history WHERE eid='$eid' AND email='$email'" )or die('Error98');
-$rowcount=mysqli_num_rows($q12);	
+$rowcount=mysqli_num_rows($q12);  
 if($rowcount == 0){
-	echo '<tr><td>'.$c++.'</td><td>'.$title.'</td><td>'.$total.'</td><td>'.$sahi*$total.'</td><td>'.$time.'&nbsp;min</td>
-	<td><b><a href="account.php?q=quiz&step=2&eid='.$eid.'&n=1&t='.$total.'" class="pull-right btn sub1" style="margin:0px;background:blue"><span class="glyphicon glyphicon-new-window" aria-hidden="true"></span>&nbsp;<span class="title1"><b>Start</b></span></a></b></td></tr>';
+  echo '<tr><td>'.$c++.'</td><td>'.$title.'</td><td>'.$total.'</td><td>'.$sahi*$total.'</td><td>'.$time.'&nbsp;min</td>
+  <td><b><a href="account.php?q=quiz&step=2&eid='.$eid.'&n=1&t='.$total.'" class="pull-right btn sub1" style="margin:0px;background:blue"><span class="glyphicon glyphicon-new-window" aria-hidden="true"></span>&nbsp;<span class="title1"><b>Start</b></span></a></b></td></tr>';
 }
 else
 {
 echo '<tr style="color:#99cc32"><td>'.$c++.'</td><td>'.$title.'&nbsp;<span title="This quiz is already solve by you" class="glyphicon glyphicon-ok" aria-hidden="true"></span></td><td>'.$total.'</td><td>'.$sahi*$total.'</td><td>'.$time.'&nbsp;min</td>
-	<td><b><a href="update.php?q=quizre&step=25&eid='.$eid.'&n=1&t='.$total.'" class="pull-right btn sub1" style="margin:0px;background:red"><span class="glyphicon glyphicon-repeat" aria-hidden="true"></span>&nbsp;<span class="title1"><b>Restart</b></span></a></b></td></tr>';
+  <td><b><a href="update.php?q=quizre&step=25&eid='.$eid.'&n=1&t='.$total.'" class="pull-right btn sub1" style="margin:0px;background:red"><span class="glyphicon glyphicon-repeat" aria-hidden="true"></span>&nbsp;<span class="title1"><b>Restart</b></span></a></b></td></tr>';
 }
 }
 $c=0;
@@ -186,14 +188,14 @@ $r=$row['sahi'];
 $qa=$row['level'];
 echo '<tr style="color:#66CCFF"><td>Total Questions</td><td>'.$qa.'</td></tr>
       <tr style="color:#99cc32"><td>right Answer&nbsp;<span class="glyphicon glyphicon-ok-circle" aria-hidden="true"></span></td><td>'.$r.'</td></tr> 
-	  <tr style="color:red"><td>Wrong Answer&nbsp;<span class="glyphicon glyphicon-remove-circle" aria-hidden="true"></span></td><td>'.$w.'</td></tr>
-	  <tr style="color:#66CCFF"><td>Score&nbsp;<span class="glyphicon glyphicon-star" aria-hidden="true"></span></td><td>'.$s.'</td></tr>';
+    <tr style="color:red"><td>Wrong Answer&nbsp;<span class="glyphicon glyphicon-remove-circle" aria-hidden="true"></span></td><td>'.$w.'</td></tr>
+    <tr style="color:#66CCFF"><td>Score&nbsp;<span class="glyphicon glyphicon-star" aria-hidden="true"></span></td><td>'.$s.'</td></tr>';
 }
 $q=mysqli_query($con,"SELECT * FROM rank WHERE  email='$email' " )or die('Error157');
 while($row=mysqli_fetch_array($q) )
 {
 $s=$row['score'];
-echo '<tr style="color:#990000"><td>Overall Score&nbsp;<span class="glyphicon glyphicon-stats" aria-hidden="true"></span></td><td>'.$s.'</td></tr>';
+// echo '<tr style="color:#990000"><td>Overall Score&nbsp;<span class="glyphicon glyphicon-stats" aria-hidden="true"></span></td><td>'.$s.'</td></tr>';
 }
 echo '</table></div>';
 
@@ -238,22 +240,25 @@ if(@$_GET['q']== 3)
 $q=mysqli_query($con,"SELECT * FROM rank  ORDER BY score DESC " )or die('Error223');
 echo  '<div class="panel title"><div class="table-responsive">
 <table class="table table-striped title1" >
-<tr style="color:red"><td><b>Rank</b></td><td><b>Name</b></td><td><b>Gender</b></td><td><b>College</b></td><td><b>Score</b></td></tr>';
+<tr style="color:red"><td><b>Rank</b></td><td><b>Name</b></td><td><b>Score</b></td></tr>';
 $c=0;
 while($row=mysqli_fetch_array($q) )
 {
+
 $e=$row['email'];
 $s=$row['score'];
+
 $q12=mysqli_query($con,"SELECT * FROM user WHERE email='$e' " )or die('Error231');
 while($row=mysqli_fetch_array($q12) )
 {
-$name=$row['name'];
-$gender=$row['gender'];
-$college=$row['college'];
+// $name=$row_user['name'];
+// $gender=$row_user['gender'];
+// $college=$row['college'];
+
 }
 $c++;
 
-echo '<tr><td style="color:black"><b>'.$c.'</b></td><td>'.$name.'</td><td>'.$gender.'</td><td>'.$college.'</td><td>'.$s.'</td><td>';
+echo '<tr><td style="color:black"><b>'.$c.'</b></td><td>'.$name.'    </td><td>'.$s.'</td><td>';
 }
 echo '</table></div></div>';}
 ?>
@@ -289,51 +294,51 @@ echo '</table></div></div>';}
         <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
         <h4 class="modal-title" style="font-family:'typo' "><span style="color:orange">Developers</span></h4>
       </div>
-	  
+    
       <div class="modal-body">
         <p>
-		<div class="row">
-		<div class="col-md-4">
-		 <img src="image/banban.jpg" width=100 height=100 alt="Sunny Prakash Tiwari" class="img-rounded">
+    <div class="row">
+    <div class="col-md-4">
+     <img src="image/banban.jpg" width=100 height=100 alt="Sunny Prakash Tiwari" class="img-rounded">
      
-		 </div>
+     </div>
 
      <div class="col-md-4">
-		 <img src="image/banban.jpg" width=100 height=100 alt="Sunny Prakash Tiwari" class="img-rounded">
+     <img src="image/banban.jpg" width=100 height=100 alt="Sunny Prakash Tiwari" class="img-rounded">
      
-		 </div>
-
-
-
-     <div class="col-md-4">
-		 <img src="image/banban.jpg" width=100 height=100 alt="Sunny Prakash Tiwari" class="img-rounded">
-     
-		 </div>
+     </div>
 
 
 
      <div class="col-md-4">
-		 <img src="image/banban.jpg" width=100 height=100 alt="Sunny Prakash Tiwari" class="img-rounded">
+     <img src="image/banban.jpg" width=100 height=100 alt="Sunny Prakash Tiwari" class="img-rounded">
      
-		 </div>
+     </div>
 
 
 
-		 <div class="col-md-5">
-		<a href="https://www.facebook.com/" style="color:#202020; font-family:'typo' ; font-size:18px" title="Find on Facebook">VAN MORRE A. GUANGCO</a>
-		<h4 style="color:#202020; font-family:'typo' ;font-size:16px" class="title1">+91 9165063741</h4>
-		<h4 style="font-family:'typo' ">VAAC.GUANGCO.COC@PHINMAED.COM</h4>
-		<h4 style="font-family:'typo' ">INFORMATION TECHNOLOGY</h4></div></div>
-		</p>
+     <div class="col-md-4">
+     <img src="image/banban.jpg" width=100 height=100 alt="Sunny Prakash Tiwari" class="img-rounded">
+     
+     </div>
+
+
+
+     <div class="col-md-5">
+    <a href="https://www.facebook.com/" style="color:#202020; font-family:'typo' ; font-size:18px" title="Find on Facebook">VAN MORRE A. GUANGCO</a>
+    <h4 style="color:#202020; font-family:'typo' ;font-size:16px" class="title1">+91 9165063741</h4>
+    <h4 style="font-family:'typo' ">VAAC.GUANGCO.COC@PHINMAED.COM</h4>
+    <h4 style="font-family:'typo' ">INFORMATION TECHNOLOGY</h4></div></div>
+    </p>
       </div>
 
 
       <div class="col-md-5">
-		<a href="https://www.facebook.com/" style="color:#202020; font-family:'typo' ; font-size:18px" title="Find on Facebook">VAN MORRE A.GUANGCO</a>
-		<h4 style="color:#202020; font-family:'typo' ;font-size:16px" class="title1">09972664749</h4>
-		<h4 style="font-family:'typo' ">VAAC.GUANGCO.COC@PHINMAED.COM</h4>
-		<h4 style="font-family:'typo' ">INFORMATION TECHNOLOGY</h4></div></div>
-		</p>
+    <a href="https://www.facebook.com/" style="color:#202020; font-family:'typo' ; font-size:18px" title="Find on Facebook">VAN MORRE A.GUANGCO</a>
+    <h4 style="color:#202020; font-family:'typo' ;font-size:16px" class="title1">09972664749</h4>
+    <h4 style="font-family:'typo' ">VAAC.GUANGCO.COC@PHINMAED.COM</h4>
+    <h4 style="font-family:'typo' ">INFORMATION TECHNOLOGY</h4></div></div>
+    </p>
       </div> -->
 
 
@@ -344,7 +349,7 @@ echo '</table></div></div>';}
 <!-- </div>/.modal -->
 
 <!--Modal for admin login-->
-	 <!-- <div class="modal fade" id="login">
+   <!-- <div class="modal fade" id="login">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
